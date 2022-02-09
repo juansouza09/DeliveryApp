5a9e37f5a9c32270d5dbe141e120a21156bc57d2
juanpablo9 <juan.souza@dextra-sw.com>
2022-02-09 19:53:07 -0300

    criação das checkout

--- app/src/main/java/com/example/appdelivery/MainActivity.kt
+++ app/src/main/java/com/example/appdelivery/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.dashboard_screen)
+        setContentView(R.layout.tela_sem_internet)
 
     }
 }
