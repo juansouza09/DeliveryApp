5381ea2b2be581552052f1380028c27ce7d7c2b2
Juan Pablo Barbosa de Souza <CIANDT\juan.souza@lnb035342spo.ciandt.global>
2022-02-23 15:37:18 -0300

    id renamed

--- app/src/main/java/com/example/appdelivery/tabLayoutHome/fragments/CombosFragment.kt
+++ app/src/main/java/com/example/appdelivery/tabLayoutHome/fragments/CombosFragment.kt
@@ -15,6 +15,6 @@ class CombosFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_combos, container, false)
+        return inflater.inflate(R.layout.fragment_combo, container, false)
     }
 }
